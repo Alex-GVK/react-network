b19466bee9111251629b9dae5f37ac2fcab37460
Alex-GVK <sanikgvk@gmail.com>
2021-12-21 19:00:28 +0200

    creare modules css

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
         <img src="https://images.pexels.com/photos/5232649/pexels-photo-5232649.jpeg?auto=compress&cs=tinysrgb&fit=crop&fp-y=0.54&h=500&sharp=20&w=1400"></img>
       </div>
@@ -10,9 +11,9 @@ const Profile = () => {
       <div>
         My posts
         <div>New posts</div>
-        <div>
-          <div>post 1</div>
-          <div>post 2</div>
+        <div className = {s.posts}>
+          <div className = {s.item}>post 1</div>
+          <div className = {s.item}>post 2</div>
         </div>
       </div>
     </div>
