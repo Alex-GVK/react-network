d4a0d6b3c2dd3d48fb6d7b936293f84a4481f24b
Alex-GVK <sanikgvk@gmail.com>
2021-12-21 16:42:17 +0200

    create components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://images.pexels.com/photos/5232649/pexels-photo-5232649.jpeg?auto=compress&cs=tinysrgb&fit=crop&fp-y=0.54&h=500&sharp=20&w=1400"></img>
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New posts</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
